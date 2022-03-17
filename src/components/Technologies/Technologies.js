bf90f5d0bb069a0ebe2ebea331db7921d47880a0
wiily <willyhuang18@gmail.com>
2022-03-17 16:32:14 -0700

    compeleted the tech section

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -4,9 +4,45 @@ import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
 const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+  <Section id="tech">
+    <SectionDivider />
+    <SectionTitle>Technologies </SectionTitle>
+    <SectionText>
+    Blalalala 
+    </SectionText>
+    <List>
+      <ListItem>
+        <DiReact size="4rem" />
+        <ListContainer>
+          <ListTitle>Front-End</ListTitle>
+          <ListParagraph>
+            Experience with <br/>
+            React.js
+          </ListParagraph>
+        </ListContainer>        
+      </ListItem>
+      <ListItem>
+        <DiFirebase size="4rem" />
+        <ListContainer>
+          <ListTitle>Back-End</ListTitle>
+          <ListParagraph>
+            Experience with <br/>
+            Node.js and Database
+          </ListParagraph>
+        </ListContainer>        
+      </ListItem>
+      <ListItem>
+        <DiZend size="4rem" />
+        <ListContainer>
+          <ListTitle>UI/UX</ListTitle>
+          <ListParagraph>
+            Experience with <br/>
+            Figma
+          </ListParagraph>
+        </ListContainer>        
+      </ListItem>
+    </List>
+  </Section>
 );
 
 export default Technologies;
