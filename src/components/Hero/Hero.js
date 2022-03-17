e64c83fa38037688de841b1e1672b12318abc227
wiily <willyhuang18@gmail.com>
2022-03-16 23:15:55 -0700

    adding the welcome text for viewer

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,17 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome to <br/> My Personal portfolio
+      </SectionTitle>
+      <SectionText>
+        I am Web Developer
+      </SectionText>
+      <Button onClick={() => window.location="https://github.com/willyhuang18"}>Learn More</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
