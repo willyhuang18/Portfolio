f087adb765390a8c959c8304e57994d0a2d5037c
wiily <willyhuang18@gmail.com>
2022-03-17 22:47:20 -0700

    adding the footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,9 +6,34 @@ import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTi
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <FooterWrapper>
+      <LinkList>
+        <LinkColumn>
+          <LinkTitle>Call</LinkTitle>
+          <LinkItem href='tel:111-111-1111'>111-111-1111</LinkItem>
+        </LinkColumn>
+        <LinkColumn>
+          <LinkTitle>Email</LinkTitle>
+          <LinkItem href='mail:willyhuang18@gmail.com'>willyhuang18@gmail.com</LinkItem>
+        </LinkColumn>
+      </LinkList>
+      <SocialIconsContainer>
+        <CompanyContainer>
+          <Slogan>Innovating one project at a time</Slogan>
+        </CompanyContainer>
+        <SocialContainer>
+        <SocialIcons href='https://github.com'>
+          <AiFillGithub size="3rem" />
+        </SocialIcons>
+        <SocialIcons href='https://github.com'>
+          <AiFillInstagram size="3rem" />
+        </SocialIcons>
+        <SocialIcons href='https://github.com'>
+          <AiFillLinkedin size="3rem" />
+        </SocialIcons>
+        </SocialContainer>
+      </SocialIconsContainer>
+    </FooterWrapper>
   );
 };
 
