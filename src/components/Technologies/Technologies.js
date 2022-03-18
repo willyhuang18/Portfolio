08f2b42891a3a744f37771413a3ed48471fe59df
wiily <willyhuang18@gmail.com>
2022-03-17 18:16:53 -0700

    adding About me section

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -6,6 +6,7 @@ import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './Techn
 const Technologies = () =>  (
   <Section id="tech">
     <SectionDivider />
+    <br />
     <SectionTitle>Technologies </SectionTitle>
     <SectionText>
     Blalalala 
